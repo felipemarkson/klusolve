7c8962e03761c9d440cbd4ca7aa65e1564a4b30a
Felipe Markson dos Santos Monteiro <47871564+felipemarkson@users.noreply.github.com>
2022-05-05 15:55:31 -0300

    MacOS support (#2)
    - MacOS support
    - Better CI

--- .github/workflows/build.py
+++ .github/workflows/build.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import platform
 
 ITEMS = [
     "LICENSE",
@@ -13,13 +14,26 @@ FOLDERS = [
 
 FOLDER = "klusolvex"
 
-os.environ['KLU_AS_STATIC'] = "TRUE"
-os.system("cmake -DUSE_SYSTEM_SUITESPARSE=OFF -DUSE_SYSTEM_EIGEN3=OFF")
-os.system("cmake --build . --config Release")
 
+sys = platform.system()
+os.environ['KLU_AS_STATIC'] = "TRUE"
+
+if os.path.exists(FOLDER):
+    shutil.rmtree(FOLDER)
+
+if not os.path.exists('build'):
+    os.makedirs('build')
+
+os.chdir("build")
+os.system("cmake -DUSE_SYSTEM_SUITESPARSE=OFF -DUSE_SYSTEM_EIGEN3=OFF ..")
+os.system("cmake --build . --config Release")
+os.chdir("..")
 
 for folder in FOLDERS:
     shutil.copytree(folder, os.path.join(FOLDER, folder))
 
 for item in ITEMS:
     shutil.copyfile(item, os.path.join(FOLDER, item))
+
+shutil.make_archive(f"klusolvex_felipe_static_{sys.lower()}_x64", 'zip', FOLDER)
+
